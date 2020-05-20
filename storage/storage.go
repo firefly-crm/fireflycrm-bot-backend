bf3211b5b9b6e08901ae292a3f3849381eec0967
Aleksandr Moshkin <darthramone@icloud.com>
2020-05-20 07:18:23 +0300

    UpdateOrderMessageDisplayMode fix params order

--- storage/storage.go
+++ storage/storage.go
@@ -42,7 +42,7 @@ type (
 		SetActivePaymentId(ctx context.Context, orderId, paymentId uint64) error
 		GetActiveOrderMessageIdForUser(ctx context.Context, userId uint64) (uint64, error)
 		GetOrderMessage(ctx context.Context, userId, messageId uint64) (types.OrderMessage, error)
-		UpdateOrderMessageDisplayMode(ctx context.Context, messageId, userId uint64, mode types.DisplayMode) error
+		UpdateOrderMessageDisplayMode(ctx context.Context, userId, messageId, uint64, mode types.DisplayMode) error
 		UpdateCustomerInstagram(ctx context.Context, instagram string, orderId uint64) (uint64, error)
 		UpdateCustomerPhone(ctx context.Context, phone string, orderId uint64) (uint64, error)
 		GetUser(ctx context.Context, userId uint64) (user types.User, err error)
