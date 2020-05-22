16a2123dff20eb79decf155ec9935559734a6acb
Aleksandr Moshkin <darthramone@icloud.com>
2020-05-22 08:10:21 +0300

    WhatsApp test

--- types/types.go
+++ types/types.go
@@ -265,6 +265,8 @@ func (o Order) getFullMessageString(c *Customer) string {
 
 		if c.Phone.Valid {
 			result += fmt.Sprintf("\n<b>Телефон:</b> <a href=\"https://wa.me/%s\">%s</a>", c.Phone.String, formatPhone(c.Phone.String))
+			result += fmt.Sprintf("\n<b>Телефон2:</b> <a href=\"whatsapp://%s\">%s</a>", c.Phone.String, formatPhone(c.Phone.String))
+			result += fmt.Sprintf("\n<b>WhatsApp:</b> <a href=\"whatsapp://%s\">%s</a>", c.Phone.String, "открыть")
 		}
 
 		if c.Instagram.Valid {
